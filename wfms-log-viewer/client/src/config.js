61f60c54f4d9e7bf0cb297736976ffca5a904ea4
Amit <92435410+amit8299700148@users.noreply.github.com>
2025-07-31 21:20:00 +0530

    Update config.js

--- wfms-log-viewer/client/src/config.js
+++ wfms-log-viewer/client/src/config.js
@@ -5,7 +5,7 @@ const config = {
   },
   production: {
     // Use the UI domain - Nginx will proxy /api/ requests to the backend
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://logreader-server.marketplace.svc.cluster'
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://logreader-server.prod-we.com'
   }
 };
 
