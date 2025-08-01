dbec443f69b3b56cbd8127607fe827fe96e1b57e
Prakher Srivastava <prakhersrivastava@MAC-Com-01-24-0073.local>
2025-08-01 11:14:38 +0530

    updated the port of servers to 80

--- wfms-log-viewer/client/src/config.js
+++ wfms-log-viewer/client/src/config.js
@@ -5,7 +5,7 @@ const config = {
   },
   production: {
     // Use the UI domain - Nginx will proxy /api/ requests to the backend
-    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://logreader-server.marketplace.svc.cluster.local'
+    API_BASE_URL: process.env.REACT_APP_API_URL || 'http://logreader-ui.prod-we.com'
   }
 };
 
